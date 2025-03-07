3d34c0efd18752533c54a0a16707ad3570121ba5
Matheus Thiago Hamilko Bacanof <141736930+hamilko1@users.noreply.github.com>
2025-03-06 23:20:50 -0300

    Add files via upload

---
+++ curso-alura-validação-de-voz/app/sortear.js
@@ -0,0 +1,13 @@
+const numeroSecreto = gerarNumeroAleatorio();
+const menorValor = 1;
+const maiorValor = 100;
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1)
+}
+
+const elementoMenorValor = document.getElementById('menor-valor');
+elementoMenorValor.innerHTML = menorValor
+
+const elementoMaiorValor = document.getElementById('maior-valor');
+elementoMaiorValor.innerHTML = maiorValor
